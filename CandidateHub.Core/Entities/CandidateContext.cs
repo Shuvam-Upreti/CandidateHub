0a1b645a9ea3cf4295d279174efb760acf10f8da
shuvam <shuva@DESKTOP-RU86RCC>
2024-10-19 12:17:48 +0545

    feat: added candidatedetail model

--- CandidateHub.Core/Entities/CandidateContext.cs
+++ CandidateHub.Core/Entities/CandidateContext.cs
@@ -11,4 +11,29 @@ public partial class CandidateContext : DbContext
     {
     }
 
+    public virtual DbSet<CandidateDetail> CandidateDetails { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CandidateDetail>(entity =>
+        {
+            entity.HasKey(e => e.CandidateId).HasName("PK__Candidat__DF539B9C9FD42303");
+
+            entity.ToTable("CandidateDetail");
+
+            entity.HasIndex(e => e.Email, "UQ__Candidat__A9D1053462AE9A2F").IsUnique();
+
+            entity.Property(e => e.CallTimeInterval).HasMaxLength(50);
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.GitHubProfileUrl).HasMaxLength(255);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.LinkedInProfileUrl).HasMaxLength(255);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ CandidateHub.Core/Entities/CandidateDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandidateHub.Core.Entities;
+
+public partial class CandidateDetail
+{
+    public int CandidateId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? CallTimeInterval { get; set; }
+
+    public string? LinkedInProfileUrl { get; set; }
+
+    public string? GitHubProfileUrl { get; set; }
+
+    public string FreeTextComment { get; set; } = null!;
+}
